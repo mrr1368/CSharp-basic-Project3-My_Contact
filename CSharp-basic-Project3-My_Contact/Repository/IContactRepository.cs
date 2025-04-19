a565243f3ea269d75c859741c23bfbed45ce2341
MR. Rajabali <mrrajabali@gmail.com>
2025-04-19 19:01:18 +0330

    Add IContactRepository interface for contact management
    Introduces the IContactRepository interface in the
    CSharp_basic_Project3_My_Contact.Repository namespace.
    This interface includes methods for selecting, searching,
    inserting, updating, and deleting contact records,
    utilizing DataTable for potential database interactions.

---
+++ CSharp-basic-Project3-My_Contact/Repository/IContactRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_basic_Project3_My_Contact.Repository
+{
+    internal interface IContactRepository
+    {
+        DataTable SelectAll();
+        DataTable SelectRow(int contactid);
+        DataTable Search(string searchText);
+        bool Insert(string firstName, string lastName, int age, string phoneNumber, string emailAddress, string address);
+        bool Update(int contactId, string firstName, string lastName, int age, string phoneNumber, string emailAddress, string address);
+        bool Delete(int contactId);
+    }
+}
