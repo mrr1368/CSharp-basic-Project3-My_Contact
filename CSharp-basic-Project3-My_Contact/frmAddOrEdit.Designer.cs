5c54e2d17c765f2752c465f3364e7845b35fc372
MR. Rajabali <mrrajabali@gmail.com>
2025-04-19 19:03:58 +0330

    Add frmAddOrEdit form for contact management
    Introduces a new form class `frmAddOrEdit` in the
    `CSharp_basic_Project3_My_Contact` namespace for adding
    and editing contact information. The form includes fields
    for first name, last name, age, phone number, email
    address, and address. The designer file sets up the layout
    and components, while the main code handles data loading,
    input validation, and submission logic. A resource file
    is also included for localization and other resources.

---
+++ CSharp-basic-Project3-My_Contact/frmAddOrEdit.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CSharp_basic_Project3_My_Contact
+{
+    partial class frmAddOrEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.NumericUpDown();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.lblAddress);
+            this.groupBox1.Controls.Add(this.txtEmailAddress);
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Controls.Add(this.txtPhoneNumber);
+            this.groupBox1.Controls.Add(this.lblPhoneNumber);
+            this.groupBox1.Controls.Add(this.txtAge);
+            this.groupBox1.Controls.Add(this.lblAge);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.lblLastName);
+            this.groupBox1.Controls.Add(this.txtFirstName);
+            this.groupBox1.Controls.Add(this.lblFirstName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 278);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "اطلاعات مخاطب";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(288, 236);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "ثبت";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(6, 112);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(357, 109);
+            this.txtAddress.TabIndex = 11;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(369, 115);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(39, 13);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "آدرس :";
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Location = new System.Drawing.Point(6, 85);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(357, 21);
+            this.txtEmailAddress.TabIndex = 9;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(369, 88);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(39, 13);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "ایمیل :";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(6, 57);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(126, 21);
+            this.txtPhoneNumber.TabIndex = 7;
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(138, 60);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(73, 13);
+            this.lblPhoneNumber.TabIndex = 6;
+            this.lblPhoneNumber.Text = "شماره تماس :";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(237, 58);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(126, 21);
+            this.txtAge.TabIndex = 5;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(369, 60);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(33, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "سن :";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(6, 30);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(126, 21);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(138, 33);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(75, 13);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "نام خانوادگی : ";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(237, 30);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(126, 21);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(369, 33);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(27, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "نام :";
+            // 
+            // frmAddOrEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 304);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmAddOrEdit";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.frmAddOrEdit_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAge)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.NumericUpDown txtAge;
+        private System.Windows.Forms.Label lblAge;
+    }
+}
---
+++ CSharp-basic-Project3-My_Contact/frmAddOrEdit.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharp_basic_Project3_My_Contact.Repository;
+using CSharp_basic_Project3_My_Contact.Services;
+
+namespace CSharp_basic_Project3_My_Contact
+{
+    public partial class frmAddOrEdit : Form
+    {
+        private readonly IContactRepository repository;
+        public int contactId = 0;
+
+        public frmAddOrEdit()
+        {
+            InitializeComponent();
+            repository = new ContactRepository();
+        }
+
+        private void frmAddOrEdit_Load(object sender, EventArgs e)
+        {
+            if (contactId == 0)
+            {
+                this.Text = "افزودن مخاطب جدید";
+            }
+            else
+            {
+                this.Text = "ویرایش مخاطب";
+                DataTable dt = repository.SelectRow(contactId);
+                txtFirstName.Text = dt.Rows[0][1].ToString();
+                txtLastName.Text = dt.Rows[0][2].ToString();
+                txtAge.Value = int.Parse(dt.Rows[0][3].ToString());
+                txtPhoneNumber.Text = dt.Rows[0][4].ToString();
+                txtEmailAddress.Text = dt.Rows[0][5].ToString();
+                txtAddress.Text = dt.Rows[0][6].ToString();
+                btnSubmit.Text = "ویرایش مخاطب";
+            }
+        }
+
+        bool ValidateInputs()
+        {
+            if (txtFirstName.Text == "")
+            {
+                MessageBox.Show("لطفا نام را وارد کنید .", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtLastName.Text == "")
+            {
+                MessageBox.Show("لطفا نام خانوادگی را وارد کنید .", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtAge.Value == 0)
+            {
+                MessageBox.Show("لطفا سن را وارد کنید .", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtPhoneNumber.Text == "")
+            {
+                MessageBox.Show("لطفا شماره تماس را وارد کنید .", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (txtEmailAddress.Text == "")
+            {
+                MessageBox.Show("لطفا ایمیل را وارد کنید .", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                bool isSuccess;
+                if (contactId == 0)
+                {
+                    isSuccess = repository.Insert(txtFirstName.Text, txtLastName.Text, (int)txtAge.Value, txtPhoneNumber.Text, txtEmailAddress.Text, txtAddress.Text);
+                }
+                else
+                {
+                    isSuccess = repository.Update(contactId, txtFirstName.Text, txtLastName.Text, (int)txtAge.Value, txtPhoneNumber.Text, txtEmailAddress.Text, txtAddress.Text);
+                }
+                if (isSuccess == true)
+                {
+                    MessageBox.Show("عملیات با موفقیت انجام شد", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("عملیات با شکست روبرو شد", "شکست", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+    }
+}
+
